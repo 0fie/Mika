a39d96350cbc6a1aaaaf42595fe318ffb2ce7f24
0fie <151028199+0fie@users.noreply.github.com>
2024-04-09 13:09:58 +0200

    fix: gleam TS parser now works

--- config/plug/treesitter/treesitter.nix
+++ config/plug/treesitter/treesitter.nix
@@ -9,8 +9,21 @@
       hash = "sha256-A0Lpsx0VFRYUWetgX3Bn5osCsLQrZzg90unGg9kTnVg=";
     };
   };
+  gleam-grammar = pkgs.tree-sitter.buildGrammar {
+    language = "gleam";
+    version = "0.0.0+rev=bcf9c45";
+    src = pkgs.fetchFromGitHub {
+      owner = "gleam-lang";
+      repo = "tree-sitter-gleam";
+      rev = "bcf9c45b56cbe46e9dac5eee0aee75df270000ac";
+      hash = "sha256-XdgPPX5gKvr4yIpMy6M7AKxaMrilt5Pzp6gUa8o+EwE=";
+    };
+  };
 in {
-  filetype.extension.nu = "nu";
+  filetype.extension = {
+    nu = "nu";
+    gleam = "gleam";
+  };
   plugins.treesitter = {
     enable = true;
     indent = true;
@@ -18,23 +31,28 @@ in {
     nixvimInjections = true;
     grammarPackages = with pkgs.vimPlugins.nvim-treesitter-parsers; [
       nu-grammar
+      gleam-grammar
       nix
       markdown
       markdown_inline
       python
-      gleam
-      tree-sitter-grammars.tree-sitter-gleam
     ];
   };
   extraFiles = {
     "/queries/nu/highlights.scm" = builtins.readFile "${nu-grammar}/queries/nu/highlights.scm";
     "/queries/nu/injections.scm" = builtins.readFile "${nu-grammar}/queries/nu/injections.scm";
+
+    "/queries/gleam/highlights.scm" = builtins.readFile "${gleam-grammar}/queries/highlights.scm";
+    "/queries/gleam-grammar/locals.scm" = builtins.readFile "${gleam-grammar}/queries/locals.scm";
+    "/queries/gleam-grammar/tags.scm" = builtins.readFile "${gleam-grammar}/queries/tags.scm";
   };
+
   extraConfigLua = ''
     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
     parser_config.nu = {
       filetype = "nu",
+      filetype = "gleam"
     }
   '';
 }

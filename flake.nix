5f6f43a77221649f13bfea040d4ff1cee798116b
0fie <151028199+0fie@users.noreply.github.com>
2024-04-12 09:53:36 +0200

    added other systems

--- flake.nix
+++ flake.nix
@@ -9,9 +9,14 @@
     };
   };
 
-  outputs = {nixvim, ...}:{
-    packages.default = nixvim.legacyPackages."x86_64-linux".makeNixvimWithModule {
-      module = ./config;
-    };
+  outputs = {nixpkgs,nixvim, ...}: let
+    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "x86_64-darwin" "i686-linux" "aarch64-linux" "aarch64-darwin"];
+    pkgsForEach = nixvim.legacyPackages;
+  in{
+    packages = forAllSystems (system: {
+      default = pkgsForEach.${system}.makeNixvimWithModule {
+        module = ./config;
+      };
+    });
   };
 }

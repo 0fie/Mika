cf5cd47aa1498b0ffc65ed4070a28b2b366f23eb
0fie <151028199+0fie@users.noreply.github.com>
2024-04-13 16:33:10 +0200

    minor refactor

--- config/default.nix
+++ config/default.nix
@@ -3,6 +3,9 @@
     ./keys.nix
     ./sets.nix
 
+    ./plug/ai/wtf.nix
+    ./plug/ai/backseat.nix
+
     ./plug/completion/cmp.nix
     ./plug/completion/lspkind.nix
     ./plug/completion/luasnip.nix
@@ -16,15 +19,12 @@
     ./plug/lsp/hlchunk.nix
     ./plug/lsp/lsp.nix
     ./plug/lsp/lspsaga.nix
-    #    ./plug/lsp/none-ls.nix
+    ./plug/lsp/none-ls.nix
     ./plug/lsp/trouble.nix
 
     ./plug/treesitter/treesitter-context.nix
     ./plug/treesitter/treesitter-textobjects.nix
     ./plug/treesitter/treesitter.nix
-
-    ./plug/ai/wtf.nix
-    ./plug/ai/backseat.nix
 
     ./plug/ui/alpha.nix
     ./plug/ui/bufferline.nix
@@ -37,14 +37,12 @@
     ./plug/ui/search.nix
 
     ./plug/utils/comment.nix
+    ./plug/utils/extra.nix
     ./plug/utils/dap.nix
     ./plug/utils/flash.nix
     ./plug/utils/hardtime.nix
     ./plug/utils/illuminate.nix
-    ./plug/utils/nvim-autopairs.nix
     ./plug/utils/oil.nix
     ./plug/utils/undotree.nix
-    ./plug/utils/ufo.nix
-    ./plug/utils/whichkey.nix
   ];
 }

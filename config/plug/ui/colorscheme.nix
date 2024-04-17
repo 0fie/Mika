74bb26d3b85eaf20c09c1e27301aaf44ca487571
0fie <151028199+0fie@users.noreply.github.com>
2024-04-17 07:43:55 +0200

    refactor colorscheme.nix

--- config/plug/ui/colorscheme.nix
+++ config/plug/ui/colorscheme.nix
@@ -5,31 +5,33 @@
       light = "frappe";
       dark = "mocha";
     };
-    flavour = "mocha";
-    disableBold = false;
-    disableItalic = false;
-    disableUnderline = false;
-    transparentBackground = true;
-    integrations = {
-      cmp = true;
-      noice = true;
-      notify = true;
-      gitsigns = true;
-      which_key = true;
-      illuminate = {enabled = true;};
-      treesitter = true;
-      treesitter_context = true;
-      telescope.enabled = true;
-      indent_blankline.enabled = true;
-      mini.enabled = true;
-      native_lsp = {
-        enabled = true;
-        inlay_hints.background = true;
-        underlines = {
-          errors = ["underline"];
-          hints = ["underline"];
-          information = ["underline"];
-          warnings = ["underline"];
+    settings = {
+      flavour = "mocha";
+      disableBold = false;
+      disableItalic = false;
+      disableUnderline = false;
+      transparentBackground = true;
+      integrations = {
+        cmp = true;
+        noice = true;
+        notify = true;
+        gitsigns = true;
+        which_key = true;
+        illuminate = {enabled = true;};
+        treesitter = true;
+        treesitter_context = true;
+        telescope.enabled = true;
+        indent_blankline.enabled = true;
+        mini.enabled = true;
+        native_lsp = {
+          enabled = true;
+          inlay_hints.background = true;
+          underlines = {
+            errors = ["underline"];
+            hints = ["underline"];
+            information = ["underline"];
+            warnings = ["underline"];
+          };
         };
       };
     };
